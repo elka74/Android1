d71c78ba6764bdd3f658ba213ea162df804e8e2c
elka74 <mikheeva@1454.ru>
2020-07-27 11:06:26 +0300

    1 commit

---
+++ app/src/main/java/com/mikheeva/android1les1_hw1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mikheeva.android1les1_hw1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
